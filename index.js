623d8f0ae01a062996eaa3c1beae612b185fb6ab
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-12 16:27:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function BoardMember(name,homeState,training){
   this.training = training;
 }
 
-BoardMember.veto = function(){return "No, I must disagree"}
+BoardMember.prototype.veto = function(){return "No, I must disagree"}
 
-BoardMember.approve = function(){return "You can do that!"}
+BoardMember.prototype.approve = function(){return "You can do that!"}
 
-BoardMember.doCharity=function(){return "I like to help people"}
+BoardMember.prototype.doCharity=function(){return "I like to help people"}
 
-BoardMember.releasePressStatement= function(){
+BoardMember.prototype.releasePressStatement= function(){
   return "You will see great things from Scuber."
 }
 
-BoardMember.sayHi = function(){
+BoardMember.prototype.sayHi = function(){
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
