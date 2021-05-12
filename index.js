b1c021b2ea070818b0fa4aca63385178373d7ddf
Hazem El Sankari <hazemleb96@gmail.com>
2021-05-12 16:21:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ function BoardMember(name,homestate,training){
   this.training = training;
 }
 
-BoardMember.veto
+BoardMember.veto = function(){return "No, I must disagree"}
+
+BoardMember.approve = function(){return "You can do that!"}
+
+BoardMember.doCharity=function(){return "I like to help people"}
+
+BoardMember.
